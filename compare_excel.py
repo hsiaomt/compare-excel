a589f5cc427fb5b218c350f626e71834f2bc10fe
hsiaomt <48883743+hsiaomt@users.noreply.github.com>
2026-06-10 18:18:56 +0800

    Add files via upload

--- compare_excel.py
+++ compare_excel.py
@@ -1,21 +1,29 @@
+import sys
 import os
 import pandas as pd
 
 if __name__ == "__main__":
-    file_dir = os.path.dirname(os.path.abspath(__file__))
-    print(f"當前目錄：{file_dir}")
+    if getattr(sys, 'frozen', False):
+        # 如果是打包後的執行檔，取得執行檔所在的目錄
+        exe_dir = os.path.dirname(sys.executable)
+    else:
+        # 如果是開發中的原始碼
+        exe_dir = os.path.dirname(os.path.abspath(__file__))
+    print(f"執行檔/腳本目錄：{exe_dir}")
+
     result_file_name = 'compare_result.xlsx'
-    result_file_path = os.path.join(file_dir, result_file_name)
+    result_file_path = os.path.join(exe_dir, result_file_name)
     if os.path.exists(result_file_path):
         os.remove(result_file_path)
         print(f"{result_file_name} 已刪除")
     else:
         print(f"找不到 {result_file_name}")
+
     extensions = ('.xls', '.xlsx')
-    files = [f for f in os.listdir(file_dir) if f.endswith(extensions)]
+    files = [f for f in os.listdir(exe_dir) if f.endswith(extensions)]
     files.sort()
-    file1_path = os.path.join(file_dir, files[0])
-    file2_path = os.path.join(file_dir, files[1])
+    file1_path = os.path.join(exe_dir, files[0])
+    file2_path = os.path.join(exe_dir, files[1])
 
     xls1 = pd.read_excel(file1_path, sheet_name=None, dtype=str)
     xls2 = pd.read_excel(file2_path, sheet_name=None, dtype=str)
